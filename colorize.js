b342b9739a2610fcb7d749e22454ab52ac910b17
namph-sgn <namph.sgn@gmail.com>
2024-09-21 15:51:31 +0700

    Update code to retain color after setting

--- colorize.js
+++ colorize.js
@@ -5,28 +5,33 @@
  */
 
 function loadSettings() {
-    chrome.storage.sync.get({
-      settings: settings
-    }, function(items) { settings = items.settings; });
+  chrome.storage.sync.get(
+    {
+      settings: settings,
+    },
+    function (items) {
+      settings = items.settings;
+    }
+  );
 }
 
 colorize = () => {
   var iframe = document.getElementById("microConsole-Logs");
   if (!iframe) {
-    iframe = document
-  };
+    iframe = document;
+  }
 
   const innerDocument = iframe.contentDocument
     ? iframe.contentDocument
     : iframe.contentWindow;
-  
+
   if (!innerDocument) return false;
 
   colorizeLogGroup(innerDocument);
-}
+};
 
 function getColorsByLogLevel(logMessageText) {
-  var result = {}
+  var result = {};
   if (logMessageText.indexOf("INFO") !== -1) {
     result.color = settings.InfoColor;
     result.bgColor = settings.InfoBgColor;
@@ -37,14 +42,17 @@ function getColorsByLogLevel(logMessageText) {
     result.color = settings.ErrorColor;
     result.bgColor = settings.ErrorBgColor;
   } else if (logMessageText.indexOf("FATAL") !== -1) {
-      result.color = settings.ErrorColor;
-      result.bgColor = settings.ErrorBgColor;
+    result.color = settings.ErrorColor;
+    result.bgColor = settings.ErrorBgColor;
   } else if (logMessageText.indexOf("TRACE") !== -1) {
     result.color = settings.TraceColor;
     result.bgColor = settings.TraceBgColor;
-  } else if (logMessageText.indexOf("START") !== -1 || logMessageText.indexOf("END") !== -1) {
+  } else if (
+    logMessageText.indexOf("START") !== -1 ||
+    logMessageText.indexOf("END") !== -1
+  ) {
     result.color = settings.DebugColor;
-    result.bgColor = settings.DebugBgColor
+    result.bgColor = settings.DebugBgColor;
   } else {
     result.color = settings.DefaultColor;
     result.bgColor = settings.DefaultBgColor;
@@ -53,50 +61,33 @@ function getColorsByLogLevel(logMessageText) {
 }
 
 function colorizeLogGroup(innerDocument) {
-    const logMessages = innerDocument.querySelectorAll('[class^="awsui_row"]');
-    if (logMessages.length === 0) return false;
-    logMessages.forEach(log => {
-        const logMessageText = log.innerText;
-        if (logMessageText) {
-          let result = getColorsByLogLevel(logMessageText)
+  const logMessages = innerDocument.querySelectorAll('[class^="awsui_row"]');
+  if (logMessages.length === 0) return false;
+  logMessages.forEach((log) => {
+    const logMessageText = log.innerText;
+    if (logMessageText) {
+      let result = getColorsByLogLevel(logMessageText);
 
-          const cells = log.querySelectorAll("td");
-          if (cells.length > 0) {
-          cells.forEach(cell => {
-              cell.style.color = result.color;
-              cell.style.backgroundColor = result.bgColor;
-              cell.style.borderBottom = "1px solid #AAAAAA";
-          });
-        }
+      const cells = log.querySelectorAll("td");
+      if (cells.length > 0) {
+        cells.forEach((cell) => {
+          cell.style.setProperty('color', result.color, 'important');
+          cell.style.setProperty('background-color', result.bgColor, 'important');
+          cell.style.setProperty('border-bottom', "1px solid #AAAAAA", 'important');
+        });
       }
-    });
+    }
+  });
 }
 
-function getColorsByLogLevel(logMessageText) {
-  var result = {}
-  if (logMessageText.indexOf("INFO") !== -1) {
-    result.color = InfoColor;
-    result.bgColor = InfoBgColor;
-  } else if (logMessageText.indexOf("WARNING") !== -1) {
-    result.color = WarningColor;
-    result.bgColor = WarningBgColor;
-  } else if (logMessageText.indexOf("ERROR") !== -1) {
-    result.color = ErrorColor;
-    result.bgColor = ErrorBgColor;
-  } else if (logMessageText.indexOf("FATAL") !== -1) {
-      result.color = ErrorColor;
-      result.bgColor = ErrorBgColor;
-  } else if (logMessageText.indexOf("START") !== -1 || logMessageText.indexOf("END") !== -1) {
-    result.color = DebugColor;
-    result.bgColor = DebugBgColor;
-  } else {
-    result.color = DefaultColor;
-    result.bgColor = DefaultBgColor;
-  }
-  return result;
+if ("undefined" == typeof colorizeInterval) {
+  var colorizeInterval = setInterval(() => {
+    if (!chrome.runtime?.id) {
+      // The extension was reloaded and this script is orphaned
+      clearInterval(colorizeInterval);
+      return;
+    }
+    loadSettings();
+    window.requestAnimationFrame(colorize);
+  }, 3000);
 }
-
-const colorizeInterval = setInterval(() => {
-  loadSettings();
-  window.requestAnimationFrame(colorize);
-}, 3000);
